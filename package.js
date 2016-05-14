4ba68fc6ece43f29f47f7dfbaae3426d04a69c73
Johannes Stricker <johannesstricker@gmx.net>
2016-05-14 15:14:01 +0200

    Changed version to 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'stricker:tag-input',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'A very basic tag input field.',
   // URL to the Git repository containing the source code for this package.
