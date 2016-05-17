6066d2eea2a940bd69eb0290212c1ef441145337
Johannes Stricker <johannesstricker@gmx.net>
2016-05-17 12:44:44 +0200

    Tests for strict mode, duplicates and template add-/remove-function.

--- tests/tag-input-client-tests.js
+++ tests/tag-input-client-tests.js
@@ -38,7 +38,7 @@ Tinytest.addAsync('The tagInput Template renders all given tags.', (test, done) 
   }, 500);
 });
 
-Tinytest.addAsync('The tagInput field\'s value is correct.', (test, done) => {
+Tinytest.addAsync('The tagInput fields value is correct.', (test, done) => {
   const tags = ['tag1', 'tag2', 'tag3'];
   UI.renderWithData(Template.tagInput, {
     tags,
@@ -51,4 +51,48 @@ Tinytest.addAsync('The tagInput field\'s value is correct.', (test, done) => {
   }, 500);
 });
 
-// TODO: test that adding and removing TAGS work properly
+Tinytest.addAsync('The tagInput templates add- and remove-function work properly.', (test, done) => {
+  const view = UI.render(Template.tagInput, document.body);
+
+  Meteor.setTimeout(() => {
+    const template = view.templateInstance();
+    test.equal(template.tags.get().length, 0);
+    template.addTag('TAG1');
+    template.addTag('TAG2');
+    test.equal(template.tags.get().length, 2);
+    template.removeTag(0);
+    test.equal(template.tags.get(), ['TAG2']);
+    done();
+  }, 500);
+});
+
+Tinytest.addAsync('Strict mode works properly.', (test, done) => {
+  const view = UI.renderWithData(Template.tagInput, {
+    suggestions: ['one', 'two', 'three'],
+    strict: true,
+  }, document.body);
+
+  Meteor.setTimeout(() => {
+    const template = view._domrange.members[0].view.templateInstance();
+    test.equal(template.suggestions.get().length, 3);
+    template.addTag('TAG1');
+    test.equal(template.tags.get().length, 0);
+    template.addTag('one');
+    test.equal(template.tags.get().length, 1);
+    done();
+  }, 2000);
+});
+
+Tinytest.addAsync('Allow duplicates works properly.', (test, done) => {
+  const view = UI.renderWithData(Template.tagInput, {
+    allowDuplicates: false,
+  }, document.body);
+
+  Meteor.setTimeout(() => {
+    const template = view._domrange.members[0].view.templateInstance();
+    template.addTag('TAG1');
+    template.addTag('TAG1');
+    test.equal(template.tags.get().length, 1);
+    done();
+  }, 500);
+});
